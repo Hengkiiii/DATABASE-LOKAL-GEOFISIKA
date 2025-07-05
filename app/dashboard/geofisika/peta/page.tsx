5ae7f60ccc90ed9e2ed98eff16ad6a6cc8f7fbc0
Hengki <hengkiiii807@gmail.com>
2025-07-06 00:57:20 +0700

    Fix : Hapus Env

--- app/dashboard/geofisika/peta/page.tsx
+++ app/dashboard/geofisika/peta/page.tsx
@@ -1,10 +1,8 @@
-// ✅ file: app/dashboard/geofisika/peta/page.tsx
 "use client";
 
 import React from "react";
 import dynamic from "next/dynamic";
 
-// 🧠 Dynamic import component peta dan disable SSR
 const GempaMap = dynamic(() => import("@/components/common/GempaMap"), {
   ssr: false,
 });
